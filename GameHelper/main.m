0e0a5e9331a4df916f7ac9d418a093335c647442
quentin <quentin@chenchengs-Mac-mini.local>
2017-04-07 17:24:43 +0800

    createGame

---
+++ GameHelper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GameHelper
+//
+//  Created by quentin on 2017/4/7.
+//  Copyright © 2017年 Quentin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
